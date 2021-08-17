8df98e419bde535d45cb1e079741c5cb0cb51fea
Heather Briggs <heather.briggs@sony.com>
2021-08-17 10:53:44 +0100

    Updated automated tests to use implicit wait to prevent obscured element errors

--- TestingFacade/NC01AutoTests.py
+++ TestingFacade/NC01AutoTests.py
@@ -14,12 +14,12 @@ class NC01AutoTest:
         self.mock_registry_url = "http://127.0.0.1:5102/" # url of mock registry from test suite
         self.multipart_question_storage = {}
         self.driver = webdriver.Firefox() #selenium driver for browser
+        self.driver.implicitly_wait(10) # seconds to wait for elements to load
         # Launch browser, navigate to nmos-js and update query api url to mock registry
         self.driver.get(self.NCuT_url + "Settings")
         query_api = self.driver.find_element_by_name("queryapi")
         query_api.clear()
         query_api.send_keys(self.mock_registry_url + "x-nmos/query/v1.3")
-        time.sleep(2)
 
     def _format_device_metadata(self, label, description, id):
         """ Used to format answers based on device metadata """
@@ -31,7 +31,7 @@ class NC01AutoTest:
         resource: 'senders' 'receivers'
         """
         self.driver.find_element_by_link_text(resource).click()
-        time.sleep(2)
+        self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
 
         # Find all resources
         resources = self.driver.find_elements_by_name("label")
@@ -41,7 +41,6 @@ class NC01AutoTest:
         # loop through resources and gather ids and descriptions
         for label in resource_labels:
             self.driver.find_element_by_link_text(label).click()
-            time.sleep(2)
             resource_id = self.driver.find_element_by_name("id").text
             resource_description = self.driver.find_element_by_name("description").text
             resource_list.append(self._format_device_metadata(label, resource_description, resource_id))
@@ -64,7 +63,6 @@ class NC01AutoTest:
         # Successful browsing of the Registry will be automatically logged by the test framework.
         
         # Browse senders and receivers
-        time.sleep(2)
         self.driver.find_element_by_link_text('Senders').click()
         time.sleep(2)
         self.driver.find_element_by_link_text('Receivers').click()
@@ -78,7 +76,6 @@ class NC01AutoTest:
         """
         # The NCuT should be able to discover all the Senders that are registered in the Registry.
         # Refresh the NCuT's view of the Registry and carefully select the Senders that are available from the following list.
-        time.sleep(2)
 
         return self._find_resources("Senders")
 
@@ -88,8 +85,7 @@ class NC01AutoTest:
         """
         # The NCuT should be able to discover all the Receivers that are registered in the Registry.
         # Refresh the NCuT's view of the Registry and carefully select the Receivers that are available from the following list.
-        time.sleep(2)
-        
+
         return self._find_resources("Receivers")
 
     def test_05(self, answers, metadata):
@@ -100,7 +96,8 @@ class NC01AutoTest:
         # The NCuT should be able to discover and dynamically update all the Senders that are registered in the Registry.
         # Use the NCuT to browse and take note of the Senders that are available.
         # After the 'Next' button has been clicked one of those senders will be put 'offline'.
-        time.sleep(2)
+        
+        # Save current list of senders
         self.multipart_question_storage['test_05'] = self._find_resources("Senders")
         
         return "Next"
@@ -111,12 +108,12 @@ class NC01AutoTest:
         Second question
         """
         # Please refresh your NCuT and select the sender which has been put 'offline'
-        time.sleep(5)
         sender_list = self._find_resources("Senders")
 
         # Hmm Assuming only a one item difference always. May need to add an if len==1 check and raise an exception if not
         offline_sender = list(set(self.multipart_question_storage['test_05']) - set(sender_list))
         self.multipart_question_storage['test_05_1'] = offline_sender
+
         return offline_sender[0]
 
     def test_05_2(self, answers, metadata):
@@ -128,22 +125,21 @@ class NC01AutoTest:
         # As soon as the NCuT detects the sender has come back online please press the 'Next' button.
         # The button must be pressed within x seconds of the Sender being put back 'online'.
         # This includes any latency between the Sender being put 'online' and the NCuT updating.
-        time.sleep(5)
+
         self.driver.find_element_by_link_text("Senders").click()
+        self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
         sender_list = set()
 
         # Find all senders, keep checking until same as number of senders at start of test
         while len(sender_list) < len(self.multipart_question_storage['test_05']):
             self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
-            time.sleep(2)
             senders = self.driver.find_elements_by_name("label")
             sender_list.update([entry.text for entry in senders])
             last_sender = senders[-1].text
-            time.sleep(2)
+            time.sleep(5)
             
         # Check same sender came back
         self.driver.find_element_by_link_text(last_sender).click()
-        time.sleep(2)
         resource_id = self.driver.find_element_by_name("id").text
         resource_description = self.driver.find_element_by_name("description").text
         sender_details = self._format_device_metadata(last_sender, resource_description, resource_id)
@@ -161,9 +157,10 @@ class NC01AutoTest:
         # Additional Receivers have just been registered with the Registry, a subset of which have a connection API.
         # Please refresh your NCuT and select the Receivers that have a connection API from the list below.
         # Be aware that if your NCuT only displays Receivers which have a connection API, some of the Receivers in the following list may not be visible.
-        time.sleep(2)
+
         self.driver.find_element_by_link_text('Receivers').click()
-        time.sleep(2)
+        self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
+        
         # Find all receivers
         receivers = self.driver.find_elements_by_name("label")
         receiver_labels = [receiver.text for receiver in receivers]
@@ -172,12 +169,11 @@ class NC01AutoTest:
         # loop through receivers and check if connection tab is disabled
         for receiver in receiver_labels:
             self.driver.find_element_by_link_text(receiver).click()
-            time.sleep(2)
             receiver_id = self.driver.find_element_by_name("id").text
             receiver_description = self.driver.find_element_by_name("description").text
             
             connect_button = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.NAME, "connect"))) 
-            time.sleep(2)
+            time.sleep(3)
 
             if connect_button.get_attribute("aria-disabled") == 'false':
                 potential_answers.append(self._format_device_metadata(receiver, receiver_description, receiver_id))
@@ -203,18 +199,22 @@ class NC01AutoTest:
         receiver = metadata['receiver']
 
         self.driver.find_element_by_link_text('Receivers').click()
-        time.sleep(2)
+        self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
         self.driver.find_element_by_link_text(receiver['label']).click()
+
+        connect = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, "connect")))
         time.sleep(3)
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.NAME, "connect"))).click() 
-        time.sleep(2)
-        # Find the row containing the correct sender
+        connect.click()
+
+        # Find the row containing the correct sender and activate connection
         senders = self.driver.find_elements_by_name('label')
         row = [i for i, s in enumerate(senders) if s.text == sender['label']][0]
         activate_button = self.driver.find_elements_by_name("activate")[row]
         activate_button.click()
         time.sleep(3)
+
         active_sender = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.NAME, "sender"))) 
+
         if active_sender.text == sender['label']:
             return "Next"
         else:
@@ -232,12 +232,15 @@ class NC01AutoTest:
         receiver = metadata['receiver']
 
         self.driver.find_element_by_link_text('Receivers').click()
-        time.sleep(2)
+        self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
+
+        # Find the row for the given receiver and click the deactivate button
         receivers = self.driver.find_elements_by_name('label')
         row = [i for i, r in enumerate(receivers) if r.text == receiver['label']][0]
         deactivate_button = self.driver.find_elements_by_name("active")[row]
         deactivate_button.click()
         time.sleep(2)
+
         if deactivate_button.get_attribute('value') == "false":
             return "Next"
         else:
@@ -253,15 +256,17 @@ class NC01AutoTest:
 
         self.driver.find_element_by_link_text('Receivers').click()
         self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
-        time.sleep(2)
-        active_buttons = self.driver.find_elements_by_name('active')
+        
         # Assuming only one active receiver
+        # Find the row where the active button is true
+        active_buttons = self.driver.find_elements_by_name('active')
         active_row = [i for i, b in enumerate(active_buttons) if b.get_attribute('value') == "true"][0]
         receiver = self.driver.find_elements_by_name('label')[active_row]
         receiver_label = receiver.text
         receiver.click()
         receiver_id = self.driver.find_element_by_name("id").text
         receiver_description = self.driver.find_element_by_name("description").text
+
         return self._format_device_metadata(receiver_label, receiver_description, receiver_id)
 
     def test_09_1(self, answers, metadata):
@@ -272,16 +277,17 @@ class NC01AutoTest:
         # Use the NCuT to identify the sender currently connected to receiver x
         receiver = metadata['receiver']
         self.driver.find_element_by_link_text('Receivers').click()
-        time.sleep(2)
         self.driver.find_element_by_link_text(receiver['label']).click()
+        active = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, "active")))
         time.sleep(3)
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, "active"))).click() 
-        time.sleep(2)
+        active.click()
+
+        # Find sender from receiver's active tab
         self.driver.find_element_by_name('sender').click()
-        time.sleep(2)
         sender_label = self.driver.find_element_by_name('label').text
         sender_id = self.driver.find_element_by_name('id').text
         sender_description = self.driver.find_element_by_name('description').text
+
         return self._format_device_metadata(sender_label, sender_description, sender_id)
 
     def test_09_2(self, answers, metadata):
@@ -297,17 +303,19 @@ class NC01AutoTest:
 
         receiver = metadata['receiver']
         self.driver.find_element_by_link_text('Receivers').click()
-        time.sleep(2)
+        self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
+        
+        # Find currently active receiver
         receivers = self.driver.find_elements_by_name('label')
         active_receiver = [i for i, r in enumerate(receivers) if r.text == receiver['label']][0]
         active_button = 'true'
 
+        # Periodically refresh until active button is false
         while active_button == 'true':
             self.driver.find_element_by_css_selector("[aria-label='Refresh']").click()
-            time.sleep(3)
             active_buttons = self.driver.find_elements_by_name('active')
             active_button = active_buttons[active_receiver].get_attribute('value')
-            time.sleep(2)
+            time.sleep(5)
 
         return 'Next'
 
